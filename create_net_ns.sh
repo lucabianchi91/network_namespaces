748766605abb016f3ddfaa3b5a8e893ceccec17d
LucaB <lucabianchi91@hotmail.it>
2019-02-03 17:31:49 -0500

    fix help string

--- create_net_ns.sh
+++ create_net_ns.sh
@@ -5,12 +5,12 @@ GW_INTF=""
 OPEN_BASH=0
 OUT=0
 FIRST_SCRIPT=""
-HELP="Create Network Namespaces that can communicate between them. Options:
-    -h: display the help message
-    -n number: number of namespaces to be created
-    -i intf_name: network interface to be used as gateway (if any)
-    -b: open a new bash (terminal) for each namespace
-    -f script: run script in each namespace. If -b is passed, script is run in the new terminals
+HELP="Create Network Namespaces that can communicate between them. Options:\n
+-h: display the help message\n
+-n number: number of namespaces to be created\n
+-i intf_name: network interface to be used as gateway (if any)\n
+-b: open a new bash (terminal) for each namespace\n
+-f script: run script in each namespace. If -b is passed, script is run in the new terminals
 "
 
 # : means that the option need an argument
